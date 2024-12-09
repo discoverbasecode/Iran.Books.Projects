4ec18c4a1f1a9a55352aafb8ecad2392baadb81a
discoverbasecode <discoverbasecode@gmail.com>
2024-12-09 21:14:35 +0330

    add utils image and audio in application

---
+++ Framework/Framework.Application/FileUtils/AudioUtils.cs
@@ -0,0 +1,30 @@
+﻿using NAudio.Wave;
+
+namespace Framework.Application.FileUtils;
+
+public static class AudioUtils
+{
+
+    public static void SaveAudio(string filePath, byte[] audioData)
+    {
+        File.WriteAllBytes(filePath, audioData);
+    }
+
+    public static byte[] LoadAudio(string filePath)
+    {
+        return File.ReadAllBytes(filePath);
+    }
+
+    public static void PlayAudio(string filePath)
+    {
+        using var audioFileReader = new AudioFileReader(filePath);
+        using var outputDevice = new WaveOutEvent();
+        outputDevice.Init(audioFileReader);
+        outputDevice.Play();
+        while (outputDevice.PlaybackState == PlaybackState.Playing)
+        {
+            System.Threading.Thread.Sleep(100);
+        }
+    }
+
+}
---
+++ Framework/Framework.Application/FileUtils/ImageUtils.cs
@@ -0,0 +1,20 @@
+﻿using NAudio.Wave;
+using SixLabors.ImageSharp;
+
+namespace Framework.Application.FileUtils;
+
+public static class SoundUtils
+{
+
+    public static void SaveImage(string filePath, Image image)
+    {
+        using var fileStream = new FileStream(filePath, FileMode.Create);
+        image.SaveAsJpeg(fileStream);
+    }
+
+    public static Image LoadImage(string filePath)
+    {
+        return Image.Load(filePath);
+    }
+
+}
